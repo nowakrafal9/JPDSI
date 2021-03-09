3a09986feff3bc6929ad6c333a83e14af67dbd41
Rafal <Rafal@DESKTOP-CG1193O>
2021-03-09 12:00:12 +0100

    - Modified path for _APP_ROOT for config.php - Updated link href for stylesheet

--- Materiały/php_03_uproszczony/config.php
+++ Materiały/php_03_uproszczony/config.php
@@ -1,7 +1,7 @@
 <?php
-define('_SERVER_NAME', 'localhost:80');
+define('_SERVER_NAME', 'localhost');
 define('_SERVER_URL', 'http://'._SERVER_NAME);
-define('_APP_ROOT', '/php_03_uproszczony');
+define('_APP_ROOT', '/pliki/JPDSI/Materiały/php_03_uproszczony/');
 define('_APP_URL', _SERVER_URL._APP_ROOT);
 define("_ROOT_PATH", dirname(__FILE__));
 
--- Materiały/php_03_uproszczony/templates/top.php
+++ Materiały/php_03_uproszczony/templates/top.php
@@ -5,7 +5,7 @@
 	<meta name="viewport" content="width=device-width, initial-scale=1.0">
 	<meta name="description" content="<?php out($page_description); if (!isset($page_description)) {  ?> Opis domyślny ... <?php } ?>">
 	<title><?php out($page_title); if (empty($page_title)) {  ?> Tytuł domyślny ... <?php } ?></title>
-	<link rel="stylesheet" href="https://unpkg.com/purecss@0.6.2/build/pure-min.css" integrity="sha384-UQiGfs9ICog+LwheBSRCt1o5cbyKIHbwjWscjemyBMT9YCUMZffs6UqUTd0hObXD" crossorigin="anonymous">
+	<link rel="stylesheet" href="https://unpkg.com/purecss@2.0.5/build/pure-min.css" integrity="sha384-LTIDeidl25h2dPxrB2Ekgc9c7sEC3CWGM6HeFmuDNUjX76Ert4Z4IY714dhZHPLd" crossorigin="anonymous">
 	<link rel="stylesheet" href="<?php print(_APP_URL); ?>/css/style.css">	
 </head>
 <body>
