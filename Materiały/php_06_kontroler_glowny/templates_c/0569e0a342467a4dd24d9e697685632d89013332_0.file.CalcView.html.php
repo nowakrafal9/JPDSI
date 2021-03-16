dcf0c8c0139efa5193a7eee7d04eb4d2f34cb6b9
Rafal <Rafal@DESKTOP-CG1193O>
2021-03-16 08:23:34 +0100

    Added sample files for Lab5

---
+++ Materiały/php_06_kontroler_glowny/templates_c/0569e0a342467a4dd24d9e697685632d89013332_0.file.CalcView.html.php
@@ -0,0 +1,148 @@
+<?php
+/* Smarty version 3.1.30, created on 2021-03-16 08:16:20
+  from "D:\xampp\htdocs\pliki\php\php_06_kontroler_glowny\app\calc\CalcView.html" */
+
+/* @var Smarty_Internal_Template $_smarty_tpl */
+if ($_smarty_tpl->_decodeProperties($_smarty_tpl, array (
+  'version' => '3.1.30',
+  'unifunc' => 'content_60505b44a61812_12249803',
+  'has_nocache_code' => false,
+  'file_dependency' => 
+  array (
+    '0569e0a342467a4dd24d9e697685632d89013332' => 
+    array (
+      0 => 'D:\\xampp\\htdocs\\pliki\\php\\php_06_kontroler_glowny\\app\\calc\\CalcView.html',
+      1 => 1615829785,
+      2 => 'file',
+    ),
+  ),
+  'includes' => 
+  array (
+  ),
+),false)) {
+function content_60505b44a61812_12249803 (Smarty_Internal_Template $_smarty_tpl) {
+$_smarty_tpl->_loadInheritance();
+$_smarty_tpl->inheritance->init($_smarty_tpl, true);
+?>
+
+
+<?php 
+$_smarty_tpl->inheritance->instanceBlock($_smarty_tpl, 'Block_32774066960505b449607d4_32795285', 'footer');
+?>
+
+
+<?php 
+$_smarty_tpl->inheritance->instanceBlock($_smarty_tpl, 'Block_43146929860505b44a60f80_02066634', 'content');
+$_smarty_tpl->inheritance->endChild();
+$_smarty_tpl->_subTemplateRender(($_smarty_tpl->tpl_vars['conf']->value->root_path).("/templates/main.html"), $_smarty_tpl->cache_id, $_smarty_tpl->compile_id, 0, $_smarty_tpl->cache_lifetime, array(), 2, true);
+}
+/* {block 'footer'} */
+class Block_32774066960505b449607d4_32795285 extends Smarty_Internal_Block
+{
+public function callBlock(Smarty_Internal_Template $_smarty_tpl) {
+?>
+przykładowa tresć stopki wpisana do szablonu głównego z szablonu kalkulatora<?php
+}
+}
+/* {/block 'footer'} */
+/* {block 'content'} */
+class Block_43146929860505b44a60f80_02066634 extends Smarty_Internal_Block
+{
+public function callBlock(Smarty_Internal_Template $_smarty_tpl) {
+?>
+
+
+<h2 class="content-head is-center">Prosty kalkulator</h2>
+
+<div class="pure-g">
+<div class="l-box-lrg pure-u-1 pure-u-med-2-5">
+	<form class="pure-form pure-form-stacked" action="<?php echo $_smarty_tpl->tpl_vars['conf']->value->action_root;?>
+calcCompute" method="post">
+		<fieldset>
+
+			<label for="x">Pierwsza liczba</label>
+			<input id="x" type="text" placeholder="wartość x" name="x" value="<?php echo $_smarty_tpl->tpl_vars['form']->value->x;?>
+">
+
+			<label for="op">Operacja</label>
+					<select id="op" name="op">
+
+<?php if (isset($_smarty_tpl->tpl_vars['res']->value->op_name)) {?>
+<option value="<?php echo $_smarty_tpl->tpl_vars['form']->value->operation;?>
+">ponownie: <?php echo $_smarty_tpl->tpl_vars['res']->value->op_name;?>
+</option>
+<option value="" disabled="true">---</option>
+<?php }?>
+						<option value="plus">(+) dodaj</option>
+						<option value="minus">(-) odejmij </option>
+						<option value="times">(*) pomnóż</option>
+						<option value="div">(/) podziel</option>
+					</select>
+					
+			<label for="y">Druga liczba</label>
+			<input id="y" type="text" placeholder="wartość y" name="y" value="<?php echo $_smarty_tpl->tpl_vars['form']->value->y;?>
+">
+
+			<button type="submit" class="pure-button">Oblicz</button>
+		</fieldset>
+	</form>
+</div>
+
+<div class="l-box-lrg pure-u-1 pure-u-med-3-5">
+
+
+<?php if ($_smarty_tpl->tpl_vars['msgs']->value->isError()) {?>
+	<h4>Wystąpiły błędy: </h4>
+	<ol class="err">
+	<?php
+$_from = $_smarty_tpl->smarty->ext->_foreach->init($_smarty_tpl, $_smarty_tpl->tpl_vars['msgs']->value->getErrors(), 'err');
+if ($_from !== null) {
+foreach ($_from as $_smarty_tpl->tpl_vars['err']->value) {
+?>
+	<li><?php echo $_smarty_tpl->tpl_vars['err']->value;?>
+</li>
+	<?php
+}
+}
+$_smarty_tpl->smarty->ext->_foreach->restore($_smarty_tpl);
+?>
+
+	</ol>
+<?php }?>
+
+
+<?php if ($_smarty_tpl->tpl_vars['msgs']->value->isInfo()) {?>
+	<h4>Informacje: </h4>
+	<ol class="inf">
+	<?php
+$_from = $_smarty_tpl->smarty->ext->_foreach->init($_smarty_tpl, $_smarty_tpl->tpl_vars['msgs']->value->getInfos(), 'inf');
+if ($_from !== null) {
+foreach ($_from as $_smarty_tpl->tpl_vars['inf']->value) {
+?>
+	<li><?php echo $_smarty_tpl->tpl_vars['inf']->value;?>
+</li>
+	<?php
+}
+}
+$_smarty_tpl->smarty->ext->_foreach->restore($_smarty_tpl);
+?>
+
+	</ol>
+<?php }?>
+
+<?php if (isset($_smarty_tpl->tpl_vars['res']->value->result)) {?>
+	<h4>Wynik</h4>
+	<p class="res">
+	<?php echo $_smarty_tpl->tpl_vars['res']->value->result;?>
+
+	</p>
+<?php }?>
+
+</div>
+</div>
+
+<?php
+}
+}
+/* {/block 'content'} */
+}
