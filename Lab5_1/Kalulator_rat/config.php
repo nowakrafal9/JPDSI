c33b66d9d043de0006a33c28b3fa668692ccd28c
Rafał Nowak <72728676+nowakrafal9@users.noreply.github.com>
2021-03-16 09:35:45 +0100

    Update config.php

--- Lab5_1/Kalulator_rat/config.php
+++ Lab5_1/Kalulator_rat/config.php
@@ -6,6 +6,6 @@
     $conf->root_path = dirname(__FILE__);
     $conf->server_name = 'localhost';
     $conf->server_url = 'http://'.$conf->server_name;
-    $conf->app_root = '/pliki/JPDSI/Lab5_1';
+    $conf->app_root = '/pliki/JPDSI/Lab5_1/Kalkulator_rat';
     $conf->app_url = $conf->server_url.$conf->app_root;
 
