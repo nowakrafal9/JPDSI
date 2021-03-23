f8518052e6c19d7c9c25d238be229fdb18b785c3
Rafal <Rafal@DESKTOP-CG1193O>
2021-03-23 08:20:02 +0100

    Added sample files for Lab6

--- Materiały/php_06_kontroler_glowny/config.php
+++ Materiały/php_06_kontroler_glowny/config.php
@@ -6,7 +6,7 @@ $conf = new Config();
 $conf->root_path = dirname(__FILE__);
 $conf->server_name = 'localhost:80';
 $conf->server_url = 'http://'.$conf->server_name;
-$conf->app_root = '/php_06_kontroler_glowny';
+$conf->app_root = '/pliki/JPDSI/Materiały/php_06_kontroler_glowny';
 $conf->app_url = $conf->server_url.$conf->app_root;
 $conf->action_root = $conf->app_root.'/app/ctrl.php?action=';
 $conf->action_url = $conf->server_url.$conf->action_root;
