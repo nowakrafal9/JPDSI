29d82b87da34ecf3b6be801cf9ee610a58553b7e
Rafal <Rafal@DESKTOP-CG1193O>
2021-04-11 16:03:42 +0200

    Added Lab7_2

---
+++ Lab7_2/Kalkulator_rat/init.php
@@ -0,0 +1,43 @@
+<?php
+    require_once 'core/Config.class.php';
+    $conf = new core\Config();
+    require_once 'config.php';
+
+    function &getConf(){ global $conf; return $conf; }
+    
+    require_once getConf()->root_path.'/core/Messages.class.php';
+    $messages = new core\Messages();
+    
+    function &getMessages(){ global $messages; return $messages; }
+    
+    $smarty = null;	
+    function &getSmarty(){
+	global $smarty;
+	if (!isset($smarty)){
+		include_once getConf()->root_path.'/lib/smarty/Smarty.class.php';
+		$smarty = new Smarty();	
+
+		$smarty->assign('conf',getConf());
+		$smarty->assign('messages',getMessages());
+
+		$smarty->setTemplateDir(array(
+			'one' => getConf()->root_path.'/app/view',
+			'two' => getConf()->root_path.'/app/view/templates'
+		));
+	}
+	return $smarty;
+    }
+    
+    require_once 'core/ClassLoader.class.php';
+    $cloader = new core\ClassLoader();
+    function &getLoader() {
+        global $cloader;
+        return $cloader;
+    }
+
+    require_once 'core/functions.php';
+
+    session_start(); 
+    $conf->roles = isset($_SESSION['_roles']) ? unserialize($_SESSION['_roles']) : array();
+
+    $action = getFromRequest('action');
