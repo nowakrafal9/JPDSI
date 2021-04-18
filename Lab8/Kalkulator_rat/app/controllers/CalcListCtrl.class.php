24a1deff85baa96f39fb9085c2dc21123d32570d
Rafal <Rafal@DESKTOP-CG1193O>
2021-04-18 17:55:05 +0200

    Modified Lab8

--- Lab8/Kalkulator_rat/app/controllers/CalcListCtrl.class.php
+++ Lab8/Kalkulator_rat/app/controllers/CalcListCtrl.class.php
@@ -80,7 +80,7 @@
 
             getSmarty()->assign('page_title','Kalkulator rat');
             getSmarty()->assign('page_description','Kalkulator rat obiektowy');
-            getSmarty()->assign('page_header','<h1 class="lead">Kalkulator rat</h1> <p class="tagline">Ochrona zasobów, routing</p>');
+            getSmarty()->assign('page_header','<h1 class="lead">Kalkulator rat</h1> <p class="tagline">Połączenie z bazą danych</p>');
             getSmarty()->assign('page_footer','Kalkulator rat oparty na szablonie <a href="https://www.gettemplate.com/info/progressus/">Progressus</a>');         
 
             getSmarty()->assign('form', $this->form);
